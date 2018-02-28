b03698377f49a4f06a2d5e2da82109cda8854549
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2018-02-28 14:00:56 -0800

    parse_output ready to use

--- parse_output.py
+++ parse_output.py
@@ -4,54 +4,24 @@ import os
 import scipy.interpolate as interp
 import scipy.optimize as sciopt
 
-def parse_output(self, output_path):
-
-    raw_output = None
-    for file in os.listdir(output_path):
-        if file.endswith(".raw"):
-            raw_output = os.path.join(output_path, file)
-    if raw_output is None:
-        print("something wrong with raw data:%s" % (output_path))
-        return
-
-    ac_fname = raw_output + "/ac.ac"
-    dc_fname = raw_output + "/dcOp.dc"
-    sci_notation = "[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?"
-    freq_regex_pattern = r'\"(freq)\"\s*(%s)' % sci_notation
-    vout_regex_pattern = r'\"vd\" \((%s) (%s)\)' % (sci_notation, sci_notation)
-    ibias_regex_pattern = r'\"VVDD:p\" \"I\" (%s)' % sci_notation
-    freq_regex = re.compile(freq_regex_pattern)
-    vout_regex = re.compile(vout_regex_pattern)
-    ibias_regex = re.compile(ibias_regex_pattern)
-
-    freq = list()
-    vout = list()
-    ibias = None
+def parse_output(output_path):
+    ac_fname = os.path.join(output_path, 'ac.csv')
+    dc_fname = os.path.join(output_path, 'dc.csv')
 
     if not os.path.isfile(ac_fname) or not os.path.isfile(dc_fname):
-        print("ac/dc file doesn't exist: %s" % raw_output)
+        print("ac/dc file doesn't exist: %s" % output_path)
 
-    else:
-        ac_file = open(ac_fname, 'r')
-        for line in ac_file:
-            freq_found = freq_regex.search(line)
-            vout_found = vout_regex.search(line)
-            if freq_found:
-                freq.append(float(freq_found.group(2)))
-            elif vout_found:
-                imag = float(vout_found.group(3))
-                real = float(vout_found.group(1))
-                vout.append(real + imag * 1j)
+    ac_raw_outputs = np.genfromtxt(ac_fname, skip_header=1)
+    dc_raw_outputs = np.genfromtxt(dc_fname, skip_header=1)
+    print (dc_raw_outputs)
+    freq = ac_raw_outputs[:, 0]
+    vout = ac_raw_outputs[:, 1]
+    ibias = -dc_raw_outputs[1]
 
-        ac_file.close()
-        freq = np.array(freq)
-        vout = np.array(vout)
+    print(freq.shape)
+    print(vout.shape)
+    print(ibias)
 
-        dc_file = open(dc_fname, 'r')
-        for line in dc_file:
-            ibias_found = ibias_regex.search(line)
-            if ibias_found:
-                ibias = -float(ibias_found.group(1))
-        dc_file.close()
-
-        return freq, vout, ibias
+if __name__ == '__main__':
+    output_path = os.path.curdir
+    parse_output(output_path)
